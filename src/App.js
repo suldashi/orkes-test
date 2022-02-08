5f360a57fcac83f58bb99e73a03b8f66ad912279
Ermir Suldashi <ermirsuldashi@gmail.com>
2022-02-08 22:50:39 +0100

    added search

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import InfiniteScroll from 'react-infinite-scroll-component';
 
 function App() {
 
+ 
+
   const pageLimit = 5;
 
   const [pageNr, setPageNr] = useState(0);
   const [items, setItems] = useState([]);
   const [hasMore, setHasMore] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const onSearchInput = (ev) => {
+    setSearchTerm(ev.target.value);
+    console.log(ev.target.value);
+  }
 
   const fetchData = useCallback(async () => {
     let url = "https://www.pinkvilla.com/photo-gallery-feed-page";
@@ -32,6 +40,16 @@ function App() {
   }, []);
     return (
       <div className="App">
+        <header>
+          <div className="header-item">Home</div>
+          <div className="header-item">Latest news</div>
+          <div className="header-item">Showbiz</div>
+          <div className="header-item">Gossip</div>
+          <div className="header-item header-item-search">
+            <input onChange={(ev) => onSearchInput(ev)} type='text' placeholder='Search' value={searchTerm} />
+          </div>
+          <div className="header-item">Login</div>
+        </header>
         <div className="center-column" >
         <InfiniteScroll
           dataLength={items.length}
@@ -44,7 +62,14 @@ function App() {
             </p>
           }
           >
-          {items.map(x => <ColumnItem key={x.node.nid} node={x.node} />)}
+          {items.filter(x => {
+            if(searchTerm==='') {
+              return true;
+            }
+            else {
+              return x.node.title.toLowerCase().search(searchTerm.toLowerCase()) !== -1;
+            }
+          }).map(x => <ColumnItem key={x.node.nid} node={x.node} />)}
         </InfiniteScroll>
         </div>
       </div>
